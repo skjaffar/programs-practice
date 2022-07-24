0cc2562ea208b3fe101443dff16d64f000e9ce08
Shaik Jaffar <107207838+skjaffar@users.noreply.github.com>
2022-07-24 22:27:58 +0530

    Add files via upload

---
+++ electricity.py
@@ -0,0 +1,28 @@
+"""convert temperature in to fahrenheits:"""
+
+# temp=int(input("enter the temp in celcious:" ))
+
+# fahrenheits=temp*1.8+32
+# print(fahrenheits)                    #input temp in celciousis: 100  , out put is 212.0 fahrenheits
+
+
+
+
+""" check the number is even or odd :"""
+
+# a=int(input("enter the value of a number:"))
+
+# if a % 2==0:
+#     print("entered even number")
+# else:
+#     print("entered number is odd number")
+
+
+""" euclidean distance:"""
+
+x1=float(input("enter x1 of x coodinate:"))
+y1=float(input("enter y1 of y coordinate:"))
+x2 =float(input("enter x2 of x ocordinate:"))
+y2=float(input("enter y2 of y coordinate:"))
+d= (x2-x1)**2 +((y2-y1)**2)**0.5
+print(d)
